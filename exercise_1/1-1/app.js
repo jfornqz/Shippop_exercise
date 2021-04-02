395e2bd3c7b9b20bf61de915c26297b40d88d932
Jfornqz <61070065@kmitl.ac.th>
2021-04-02 23:50:26 +0700

    commit assignment

---
+++ exercise_1/1-1/app.js
@@ -0,0 +1,8 @@
+const checkName = (firstName, lastName) => {
+    if (firstName === 'Shippop') {
+        console.log('Best Online Shipping Platform');
+    } else {
+        console.log(`Hello Shippop, My name is ${firstName} ${lastName}`);
+    };
+}
+checkName('Thotsawat', 'Jantapitak')
